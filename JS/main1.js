a6230b536c4450925247f0e2037912301175b105
JawharZinelabidine <jawharzedd@gmail.com>
2023-08-10 00:46:48 +0100

    added question lists and answer checkboxes and limited boxes user can check

--- JS/main1.js
+++ JS/main1.js
@@ -10,5 +10,26 @@ $('.subjects').hide()
 $('#'+pageName).show()
 
 
-
 })
+
+
+$('.subjects .questions').on('click','.box', function() {
+  
+    var list=($(this).parent())
+var z=(list.parent())
+    var e=z.parent().attr('id')
+
+
+    $('#'+e+' .box').not(this).prop('checked', false);
+})
+
+
+//$(this).attr('disabled', true)
+
+//$('.box').on('click', function () {
+
+//var list=($(this).parent())
+//var z=  (list.parent())
+ //console.log(z.parent().attr('id'))
+
+// })
