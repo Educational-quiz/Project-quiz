6d9e9b479db95ae2bb5178bc91afe037e8d43b8e
JawharZinelabidine <jawharzedd@gmail.com>
2023-08-10 16:59:40 +0100

    implemented scoring system

--- JS/home.js
+++ JS/home.js
@@ -1,19 +1,21 @@
 $(function () {
-    $("#logoutButton").click(function () {
-        window.location.href = "../HTML/SignIn.HTML";
-      });
-      $("#takeQuizButton").click(function () {
-        window.location.href = "../HTML/Form.HTML";
-      });
-      var users = getLocalStorageItem("users") || [];
-      users.sort(function (a, b) {
-        return b.score - a.score;
-      });
-      var leaderboardList = $("#leaderboardList");
-      for (var i = 0; i < users.length; i++) {
-        leaderboardList.append(`<li>${users[i].firstName} - Score: ${users[i].score}</li>`);
-      }
-      function getLocalStorageItem(key) {
-        return JSON.parse(localStorage.getItem(key));
-      }
+  $("#logoutButton").click(function () {
+      window.location.href = "../HTML/SignIn.HTML";
+    });
+    $("#takeQuizButton").click(function () {
+      window.location.href = "../HTML/Form.HTML";
+    });
+    var users = getLocalStorageItem("users") || [];
+    users.sort(function (a, b) {
+      return b.score - a.score;
+    });
+    var leaderboardList = $("#leaderbordList");
+    for (var i = 0; i < users.length; i++) {
+      var user = users[i];
+      console.log(user);
+      $("#leaderbordList").append("<li>"+ user.firstName + " - Score: " + user.totalScore +"</li>");
+    }
+    function getLocalStorageItem(key) {
+      return JSON.parse(localStorage.getItem(key));
+    }
 })
