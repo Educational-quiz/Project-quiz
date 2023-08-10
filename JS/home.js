0477d85fea7a13a30c40b9d3d21157a55be14d63
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-08-10 23:32:00 +0200

    styling home page

--- JS/home.js
+++ JS/home.js
@@ -9,9 +9,8 @@ $(function () {
       users.sort(function (a, b) {
         return b.score - a.score;
       });
-      var leaderboardList = $("#leaderboardList");
       for (var i = 0; i < users.length; i++) {
-        leaderboardList.append(`<li>${users[i].firstName} - Score: ${users[i].score}</li>`);
+        $("#leaderbordList").append("<li>"+ users[i].firstName + " - Score: " + users[i].score +"</li>");
       }
       function getLocalStorageItem(key) {
         return JSON.parse(localStorage.getItem(key));
