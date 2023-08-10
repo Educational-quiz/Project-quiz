a460b7db6919af1d1f79333b50004a1b1e299ce4
JawharZinelabidine <jawharzedd@gmail.com>
2023-08-10 22:14:51 +0100

    now score values are permanently saved to localStorage

--- JS/home.js
+++ JS/home.js
@@ -7,15 +7,20 @@ $(function () {
     });
     var users = getLocalStorageItem("users") || [];
     users.sort(function (a, b) {
-      return b.score - a.score;
+      return b.totalScore - a.totalScore;
     });
     var leaderboardList = $("#leaderbordList");
     for (var i = 0; i < users.length; i++) {
       var user = users[i];
       console.log(user);
-      $("#leaderbordList").append("<li>"+ user.firstName + " - Score: " + user.totalScore +"</li>");
+      leaderboardList.append("<li>"+ user.firstName + " - Score: " + user.totalScore +"</li>");
     }
     function getLocalStorageItem(key) {
       return JSON.parse(localStorage.getItem(key));
     }
 })
+
+var users = getLocalStorageItem("users") || []
+function getLocalStorageItem(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
