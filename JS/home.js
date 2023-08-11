60ff08c9eb57e71a937467302f9adbc577eabd57
JawharZinelabidine <jawharzedd@gmail.com>
2023-08-11 01:54:13 +0100

    added retake quiz button

--- JS/home.js
+++ JS/home.js
@@ -9,6 +9,10 @@ $(function () {
     users.sort(function (a, b) {
       return b.totalScore - a.totalScore;
     });
+    var ID = getLocalStorageItem('currentUser');
+    if (users[ID].totalScore !== null) {
+      $("#takeQuizButton").text("Retake Quiz");
+    }
     for (var i = 0; i < users.length; i++) {
       if(users[i].totalScore !== null) {
         $("#leaderbordList").append("<li>"+ users[i].firstName + " - Score: " + users[i].totalScore + " %" +"</li>");
@@ -17,8 +21,6 @@ $(function () {
     function getLocalStorageItem(key) {
       return JSON.parse(localStorage.getItem(key));
     }
-    var users = getLocalStorageItem("users") || []
-    var ID = getLocalStorageItem("currentUser");
     function getLocalStorageItem(key) {
       return JSON.parse(localStorage.getItem(key));
     }
